154d92a0d3d7332f5378b55c07870accb65f7cf1
Sid <mjauwe@gmail.com>
2019-09-10 21:12:19 +0200

    init commit

---
+++ precache-manifest.4e5962ab14e816a2a738e214f7231e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cb5ec6c11412021a888f40fa758d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc8a7e8b3bf57ae3be2c",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "3e568c7dd2a60fed3776",
+    "url": "/static/css/main.d6195583.chunk.css"
+  },
+  {
+    "revision": "bc8a7e8b3bf57ae3be2c",
+    "url": "/static/js/2.531bda9a.chunk.js"
+  },
+  {
+    "revision": "3e568c7dd2a60fed3776",
+    "url": "/static/js/main.3c41e0a9.chunk.js"
+  },
+  {
+    "revision": "936752e869bebea77727",
+    "url": "/static/js/runtime~main.28c0a441.js"
+  }
+]);
