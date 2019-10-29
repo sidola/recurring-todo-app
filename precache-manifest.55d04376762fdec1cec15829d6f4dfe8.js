c642eb722beeb095cea5d535fa606a4b43e7bb9d
Sid <mjauwe@gmail.com>
2019-10-29 19:23:21 +0100

    Update app, fix root domain

---
+++ precache-manifest.55d04376762fdec1cec15829d6f4dfe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c2644233c1d585e443bd7fff384114",
+    "url": "/index.html"
+  },
+  {
+    "revision": "470aaf9ceb0e0b5ffc15",
+    "url": "/static/css/2.b03de2b6.chunk.css"
+  },
+  {
+    "revision": "b1ec0acb7de2b1e4d974",
+    "url": "/static/css/main.a2be9b0b.chunk.css"
+  },
+  {
+    "revision": "470aaf9ceb0e0b5ffc15",
+    "url": "/static/js/2.555445ed.chunk.js"
+  },
+  {
+    "revision": "b1ec0acb7de2b1e4d974",
+    "url": "/static/js/main.44d0781f.chunk.js"
+  },
+  {
+    "revision": "936752e869bebea77727",
+    "url": "/static/js/runtime~main.28c0a441.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "recurringtodos.com/precache-manifest.6ebcb7f7492a7f55cafbb190ca84bd08.js"
+  "/precache-manifest.55d04376762fdec1cec15829d6f4dfe8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("recurringtodos.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
